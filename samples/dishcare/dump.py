5932de7f92a359cfd2b78739be62906de0088198
Eric Daniels <eric@erdaniels.com>
2021-11-16 17:49:54 -0500

    finish dishcare script

--- samples/dishcare/dump.py
+++ samples/dishcare/dump.py
@@ -1,11 +1,12 @@
 import asyncio
 import sys
+import time
 from typing import List
 
 from grpclib.client import Channel
 
 import gen
-from proto.api.v1.robot_pb2 import StatusRequest, StatusResponse
+from proto.api.v1.robot_pb2 import ForceMatrixMatrixRequest, ForceMatrixMatrixResponse
 from proto.api.v1.robot_grpc import RobotServiceStub
 
 async def main(args: List[str]) -> int:
@@ -19,12 +20,32 @@ async def main(args: List[str]) -> int:
     try:
         async with Channel(grpc_addr, grpc_port) as channel:
             client = RobotServiceStub(channel)
-            status = await client.Status(StatusRequest())
-            print(status)
+
+            while True:
+                async def getMatrix(name: str):
+                    now = time.time()
+                    req = ForceMatrixMatrixRequest()
+                    req.name = name
+                    resp = await client.ForceMatrixMatrix(req)
+                    print(now, '\t', name, '\t[', end='')
+                    for rowNum in range(resp.matrix.rows):
+                        print('[', end='')
+                        for colNum in range(resp.matrix.cols):
+                            print(resp.matrix.data[(rowNum*resp.matrix.cols)+colNum], end='')
+                            if colNum == resp.matrix.cols-1:
+                                continue
+                            print(',', end='')
+                        print(']', end='')
+                        if rowNum == resp.matrix.rows-1:
+                            continue
+                    print(']')
+                await getMatrix('matrix-top')
+                await getMatrix('matrix-bottom')
     except Exception as e:
         print(e, file=sys.stderr)
         return 1
     return 0
 
 if __name__ == "__main__":
-    sys.exit(asyncio.run(main(sys.argv)))
+    loop = asyncio.get_event_loop()
+    sys.exit(loop.run_until_complete((main(sys.argv))))
